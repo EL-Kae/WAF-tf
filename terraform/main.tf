4a43445845c1c34cc37c0cf12ad4e6ac5d6275fc
EL-Kae <KaestnerLeonardo@gmail.com>
2023-12-21 20:12:02 -0500

    Uploaded scripts

---
+++ terraform/main.tf
@@ -0,0 +1,370 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    fastly = {
+      source  = "fastly/fastly"
+      version = ">= 3.0.2"
+    }
+    sigsci = {
+      source = "signalsciences/sigsci"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# -----------------
+# NETWORKING
+# -----------------
+
+resource "aws_vpc" "dvna_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_subnet" "dvna_public_subnet" {
+  count             = 2
+  cidr_block        = element(["10.0.0.0/24", "10.0.1.0/24"], count.index)
+  vpc_id            = aws_vpc.dvna_vpc.id
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+}
+
+resource "aws_subnet" "dvna_private_subnet" {
+  count             = 2
+  cidr_block        = element(["10.0.2.0/24", "10.0.3.0/24"], count.index)
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+  vpc_id            = aws_vpc.dvna_vpc.id
+}
+
+resource "aws_internet_gateway" "dvna_igw" {
+  vpc_id = aws_vpc.dvna_vpc.id
+}
+
+resource "aws_eip" "dvna_ips" {
+  count      = 2
+  depends_on = [aws_internet_gateway.dvna_igw]
+}
+
+resource "aws_nat_gateway" "dvna_nat" {
+  count         = 2
+  subnet_id     = element(aws_subnet.dvna_public_subnet.*.id, count.index)
+  allocation_id = element(aws_eip.dvna_ips.*.id, count.index)
+}
+
+resource "aws_route_table" "dvna_private_route" {
+  count  = 2
+  vpc_id = aws_vpc.dvna_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.dvna_nat.*.id, count.index)
+  }
+}
+
+resource "aws_route_table" "dvna_public_route" {
+  vpc_id = aws_vpc.dvna_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dvna_igw.id
+  }
+}
+
+resource "aws_route_table_association" "dvna_private_table_association" {
+  count          = 2
+  subnet_id      = element(aws_subnet.dvna_private_subnet.*.id, count.index)
+  route_table_id = element(aws_route_table.dvna_private_route.*.id, count.index)
+}
+
+resource "aws_route_table_association" "dvna_public_table_association" {
+  count          = 2
+  subnet_id      = element(aws_subnet.dvna_public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.dvna_public_route.id
+}
+
+# -----------------
+# LOAD BALANCER
+# -----------------
+
+/*resource "aws_security_group" "dvna_https_sg" {
+  name        = "dvna-https-sg"
+  description = "Security group for https traffic."
+  vpc_id      = aws_vpc.dvna_vpc.id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow public web traffic to alb"
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow public web traffic to alb"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+}*/
+
+resource "aws_security_group" "dvna_http_sg" {
+  name        = "dvna-http-sg"
+  description = "Security group for http traffic."
+  vpc_id      = aws_vpc.dvna_vpc.id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow public web traffic to alb"
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow public web traffic to alb"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb" "dvna_lb" {
+  name                       = "dvna-lb"
+  drop_invalid_header_fields = true
+  subnets                    = aws_subnet.dvna_public_subnet.*.id
+  security_groups            = [aws_security_group.dvna_http_sg.id] //, aws_security_group.dvna_https_sg.id]
+  # enable_waf_fail_open = false
+}
+
+resource "aws_lb_target_group" "dvna_target_group" {
+  name        = "dvna-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.dvna_vpc.id
+
+  health_check {
+    matcher = "200,302"
+  }
+}
+
+resource "aws_lb_listener" "dvna_listener_http" {
+  load_balancer_arn = aws_lb.dvna_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.dvna_target_group.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "dvna_logs" {
+  name              = "aws-waf-logs-dvna"
+  retention_in_days = 365
+}
+
+resource "aws_wafv2_web_acl_association" "alb-waf" {
+  resource_arn = aws_lb.dvna_lb.arn
+  web_acl_arn  = aws_wafv2_web_acl.dvna_waf.arn
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "waf-logs" {
+  log_destination_configs = [aws_cloudwatch_log_group.dvna_logs.arn]
+  resource_arn            = aws_wafv2_web_acl.dvna_waf.arn
+}
+
+/*data "aws_acm_certificate" "dvna_cert" {
+  domain      = "viceops.net"
+  types       = ["AMAZON_ISSUED"]
+  most_recent = true
+}
+
+resource "aws_lb_listener" "dvna_listener_https" {
+  load_balancer_arn = aws_lb.dvna_lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = data.aws_acm_certificate.dvna_cert.arn
+
+  default_action {
+    target_group_arn = aws_lb_target_group.dvna_target_group.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "dvna_listener_http" {
+  load_balancer_arn = aws_lb.dvna_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}*/
+
+# -----------------
+# ECS
+# -----------------
+
+resource "aws_security_group" "dvna_ecs_sg" {
+  name        = "dvna-ecs-security-group"
+  description = "Security group for ecs service."
+  vpc_id      = aws_vpc.dvna_vpc.id
+
+  ingress {
+    description     = "Allow public web traffic to app"
+    from_port       = 9090
+    protocol        = "tcp"
+    to_port         = 9090
+    security_groups = [aws_security_group.dvna_http_sg.id] //, aws_security_group.dvna_https_sg.id]
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow public web traffic to app"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_ecs_task_definition" "dvna" {
+  family                   = "dvna_app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 2048
+  memory                   = 4096
+  container_definitions = jsonencode([
+    {
+      name         = "dvna"
+      image        = "appsecco/dvna:sqlite"
+      cpu          = 2048
+      memory       = 4096
+      network_mode = "awsvpc"
+      portMappings = [
+        {
+          containerPort = 9090
+          hostPort      = 9090
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_cluster" "dvna_cluster" {
+  name = "dvna_cluster"
+}
+
+resource "aws_ecs_service" "dvna" {
+  name            = "dvna"
+  cluster         = aws_ecs_cluster.dvna_cluster.id
+  task_definition = aws_ecs_task_definition.dvna.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups = [aws_security_group.dvna_ecs_sg.id]
+    subnets         = aws_subnet.dvna_private_subnet.*.id
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.dvna_target_group.arn
+    container_name   = "dvna"
+    container_port   = 9090
+  }
+
+  depends_on = [aws_lb_listener.dvna_listener_http] //, aws_lb_listener.dvna_listener_https]
+}
+
+# -----------------
+# FASTLY
+# -----------------
+
+provider "fastly" {
+  api_key = jsondecode(data.aws_secretsmanager_secret_version.fastly_api_key.secret_string)["api_key"]
+}
+
+resource "fastly_service_vcl" "dvna" {
+  name = "dvna"
+
+  domain {
+    name = "dvna.viceops.net"
+  }
+
+  backend {
+    address           = aws_lb.dvna_lb.dns_name
+    name              = "dvna"
+    port              = 80
+    ssl_cert_hostname = "dvna.viceops.net"
+  }
+
+  force_destroy = true
+}
+
+# -----------------
+# Signal Sciences
+# -----------------
+
+provider "sigsci" {
+  corp = "vmg"
+  # email      = "" // Required. may also provide via env variable SIGSCI_EMAIL
+  # auth_token = "" //may also provide via env variable SIGSCI_TOKEN, this or password is required
+  # password   = "" //may also provide via env variable SIGSCI_PASSWORD
+  fastly_key = jsondecode(data.aws_secretsmanager_secret_version.fastly_api_key.secret_string)["api_key"]
+}
+
+resource "sigsci_site" "dvna" {
+  short_name             = "dvna"
+  display_name           = "dvna"
+  block_duration_seconds = 1000
+  agent_anon_mode        = ""
+  agent_level            = "log"
+}
+
+resource "sigsci_edge_deployment" "dvna_deployment" {
+  site_short_name = sigsci_site.dvna.short_name
+}
+
+resource "sigsci_edge_deployment_service" "dvna-edge-service" {
+  site_short_name  = sigsci_site.dvna.short_name
+  fastly_sid       = fastly_service_vcl.dvna.id
+  activate_version = true
+  percent_enabled  = 100
+
+  depends_on = [
+    sigsci_edge_deployment.dvna_deployment
+  ]
+}
+
+# -----------------
+# OUTPUT
+# -----------------
+
+output "load_balancer_ip" {
+  description = "Outputs dns name of load balancer."
+  value       = aws_lb.dvna_lb.dns_name
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,141 @@
+data "aws_availability_zones" "available_zones" {
+  state = "available"
+}
+
+data "aws_secretsmanager_secret" "fastly_api_key" {
+  name = "dev/fastly"
+}
+
+data "aws_secretsmanager_secret_version" "fastly_api_key" {
+  secret_id = data.aws_secretsmanager_secret.fastly_api_key.id
+}
+
+# -----------------
+# WAF
+# -----------------
+
+resource "aws_wafv2_web_acl" "dvna_waf" {
+  name  = "dvna-acl"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesAdminProtectionRuleSet"
+    priority = 2
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAdminProtectionRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesAdminProtectionRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 3
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesSQLiRuleSet"
+    priority = 4
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesSQLiRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesLinuxRuleSet"
+    priority = 5
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesLinuxRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesLinuxRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "dvna-acl"
+    sampled_requests_enabled   = true
+  }
+}
